19ed4435a3d7277d19cc9c056e5eb6a954105f51
hoffm386 <erinrhof@uw.edu>
2019-06-24 14:59:26 -0700

    Done.

--- src/ColorBox.js
+++ src/ColorBox.js
@@ -1,13 +1,35 @@
 import React, { Component } from 'react';
 
 export default class ColorBox extends Component {
-  
+
+  /*
+  produceOutput = () => {
+    if (this.props.opacity >= 0.2) {
+      return <ColorBox opacity={this.props.opacity - 0.1} />
+    }
+  }*/
+
   render() {
     return (
-      <div className="color-box" style={{opacity: 2}}>
-        {/* your conditional code here! */}
+      <div className="color-box" style={{opacity: this.props.opacity}}>
+        {/* I like this format best*/}
+        {(() => {
+          if (this.props.opacity >= 0.2) {
+            return <ColorBox opacity={this.props.opacity - 0.1} />
+          }
+        })() /* <-- it's an IIFE */}
+
+        {/* You also could put it in a separate method */}
+        {/*this.produceOutput()*/}
+
+        {/* The solution branch used a ternary operator, which works, but I
+            dislike how it makes you use null.  I think an `if` with no `else`
+            is the most human-comprehensible way to write this, plus it keeps us
+            within an 80-character line width.
+        */}
+        {/*this.props.opacity >= 0.2 ?  <ColorBox opacity={/*this.props.opacity - 0.1*/} /> : null*/}
       </div>
     )
   }
-  
+
 }
